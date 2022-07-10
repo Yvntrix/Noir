440a16200208f51e6269c77edac9f06b12c30dd9
Ivanne Rencel Bano <49516553+Yvntrix@users.noreply.github.com>
2022-07-11 00:56:48 +0800

    removed boiler plate

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, MantineProvider } from "@mantine/core";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MantineProvider
+        theme={{ colorScheme: "dark" }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <Container sx={{ minHeight: "100vh" }}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </BrowserRouter>
+        </Container>
+      </MantineProvider>
+    </>
   );
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,7 @@
+import { Title } from "@mantine/core";
+
+const Home = () => {
+  return <Title>Tangina mo axie</Title>;
+};
+
+export default Home;
